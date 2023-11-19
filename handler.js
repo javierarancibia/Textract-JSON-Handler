a89d560e72b99b294335e1fe83f10e702af45849
javierarancibia <javier.arancibia.reyes@hotmail.com>
2023-11-19 21:14:39 +0100

    Table handler

--- handler.js
+++ handler.js
@@ -1,12 +1,27 @@
 const response = require('./response.js');
 
 const wordsDictionary = response.Blocks.filter((block) => block.BlockType === 'WORD').map((word) => ({ id: word.Id, confidence: word.Confidence, text: word.Text }));
-// console.log(wordsDictionary)
-
-const tableHeaders = response.Blocks.filter(block => block.BlockType === 'CELL' && block.hasOwnProperty('EntityTypes')).filter(cell => cell.EntityTypes.some(x => x === 'COLUMN_HEADER'))
-const headers = tableHeaders.map(header => header.Relationships.map(rel => rel.Ids.map(id => wordsDictionary.find(el => el.id === id ))));
-console.log(headers[2])
+const cells = response.Blocks.filter(block => block.BlockType === 'CELL')
+const cellsDictionary = cells.map(cell => ({...cell, word: wordsDictionary.find(word => word.id === cell.Relationships[0].Ids[0]) }))
+// console.log(cellsDictionary)
 
 const tables = response.Blocks.filter((block) => block.BlockType === 'TABLE');
-const structuredTables = tables.map(table => table.Relationships.map(el => ({ type: el.Type, words: el.Ids.map(id => wordsDictionary[id]) })))
-// console.log(tables)
+const structuredTables = tables.map(table => {
+    return ({
+        id: table.Id,
+        data: table.Relationships[0].Ids.map(id => ({id, cell: cellsDictionary.find(cell => cell.Id === id) }))
+    }) 
+}) 
+console.log(structuredTables[0].data)
+
+
+
+
+
+
+
+
+
+
+// const tableHeaders = response.Blocks.filter(block => block.BlockType === 'CELL' && block.hasOwnProperty('EntityTypes')).filter(cell => cell.EntityTypes.some(x => x === 'COLUMN_HEADER'))
+// const headers = tableHeaders.map(header => header.Relationships.map(rel => rel.Ids.map(id => wordsDictionary.find(el => el.id === id ))));
