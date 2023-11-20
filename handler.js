81d613d38904235c72fa392273ad34be6e998437
javierarancibia <javier.arancibia.reyes@hotmail.com>
2023-11-20 09:53:54 +0100

    returning data

--- handler.js
+++ handler.js
@@ -3,7 +3,6 @@ const response = require('./response.js');
 const wordsDictionary = response.Blocks.filter((block) => block.BlockType === 'WORD').map((word) => ({ id: word.Id, confidence: word.Confidence, text: word.Text }));
 const cells = response.Blocks.filter(block => block.BlockType === 'CELL')
 const cellsDictionary = cells.map(cell => ({...cell, word: wordsDictionary.find(word => word.id === cell.Relationships[0].Ids[0]) }))
-// console.log(cellsDictionary)
 
 const tables = response.Blocks.filter((block) => block.BlockType === 'TABLE');
 const structuredTables = tables.map(table => {
@@ -13,19 +12,14 @@ const structuredTables = tables.map(table => {
     }) 
 })
 
-// const rowedTable = structuredTables.map((table, i, array) => {
-//     if (table.EntityTypes && table.EntityTypes.some(type => type === 'COLUMN_HEADER')) {
-//         if (!array.some(x => x.type === "headers")) {
-//             return ({ type: "headers", data })
-//         }
-//         return ({ type: "headers" })
-//     }
-//     return ({
-//         rowIndex: i + 1,
-//         // data: 
-//     })
-// })
-console.log(structuredTables[0].data)
+const rowedTable = structuredTables.map(table => {
+    const headers = table.data.filter(x => 'EntityTypes'in x.cell && x.cell.RowIndex === 1)
+    const tableCells = table.data.filter(x => x.cell.RowIndex !== 1)
+    // Crear array para cada 
+    return ({ type: "table", headers, tableCells })
+})
+
+console.log(rowedTable)
 
 
 
